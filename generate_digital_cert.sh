abf16a96e9d23577a7419352f94e275e29d5904d
Develforever <rockfade@gmail.com>
2024-10-29 23:00:10 +0100

    add config

--- generate_digital_cert.sh
+++ generate_digital_cert.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -x
+set -e
+
 openssl genpkey -algorithm RSA -out digital_cert.key -pkeyopt rsa_keygen_bits:2048
 
-openssl req -new -key digital_cert.key -out digital_cert.csr
+openssl req -new -batch -key digital_cert.key -out digital_cert.csr -config digital_cert.cnf
+openssl req -in digital_cert.csr -noout -text
 
 openssl x509 -req -in digital_cert.csr -signkey digital_cert.key -out digital_cert.crt -days 365
 
